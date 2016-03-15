917514bbb2e7dd92d799c53a94efdf04bfbf8ef8
Jiri Tobolka <jiri.tobolka@bizztreat.com>
2016-03-15 16:51:30 +0100

    user role added to status file

--- lib/wrapper.rb
+++ lib/wrapper.rb
@@ -8,12 +8,12 @@ require 'securerandom'
 require 'gooddata'
 
 class UMan
-    
+
     def initialize(options)
-        
+
         @api_endpoint = 'https://syrup.keboola.com/gooddata-writer'
         @config = JSON.parse(File.read(options[:data] + '/config.json'))
-        
+
         @writer_id = @config["parameters"]["gd_writer"]
         @out_bucket = @config["parameters"]["outputbucket"]
         $set_variables = @config["parameters"]["setvariables"]
@@ -21,219 +21,219 @@ class UMan
         $gd_username = @config["parameters"]["gd_username"]
         $gd_password = @config["parameters"]["#gd_password"]
         @kbc_api_token = ENV["KBC_TOKEN"]
-        
+
         $out_file = options[:data] + '/out/tables/' + @out_bucket + '.status.csv'
-        
+
         CSV.open($out_file.to_s, "ab") do |status|
-            status << ["user", "job_id", "status", "action_done", "timestamp"]
+            status << ["user", "job_id", "status", "action_done", "timestamp", "role"]
         end
-        
+
     end
-    
+
     # get users from GoodData Project
     def get_users
-        
+
         headers  = {:x_storageapi_token => @kbc_api_token, :accept => :json, :content_type => :json}
-        
+
         response = RestClient.get "#{@api_endpoint}/users?writerId=#{@writer_id}", headers
-        
+
         return response
-        
+
     end
-    
+
     # get role IDs for specific project
     def get_project_roles(pid)
-        
+
         headers  = {:x_storageapi_token => @kbc_api_token, :accept => :json, :content_type => :json}
-        
+
         query = "/gdc/projects/#{pid}/roles"
-        
+
         response = RestClient.get "#{@api_endpoint}/proxy?writerId=#{@writer_id}&query=#{query}", headers
-        
+
         # parse key values for specific project roles
-        
+
         return response
-        
+
     end
-    
+
     #create new user in Keboola Organization
     def create_user(user,pass,firstname,lastname,sso_provider)
-        
+
         #   test_user = 'jiri.tobolka+kbc@bizztreat.com'
         #   pass = 'akbvgdrz77'
         #   firstname = 'J'
         #   lastname = 'T'
-        
+
         values   = "{\"writerId\": \"#{@writer_id}\", \"email\": \"#{user}\", \"password\": \"#{pass}\", \"firstName\": \"#{firstname}\", \"lastName\": \"#{lastname}\", \"ssoProvider\": \"#{sso_provider}\"}"
-        
+
         headers  = {:x_storageapi_token => @kbc_api_token, :accept => :json, :content_type => :json}
-        
+
         response = RestClient.post "#{@api_endpoint}/users", values, headers
-        
+
         return response
-        
+
     end
-    
+
     # does the post on projects/pid/users resource if not available sends invitation
     def add_to_project(user, role, pid)
-        
+
         headers  = {:x_storageapi_token => @kbc_api_token, :accept => :json, :content_type => :json}
-        
+
         values   = "{ \"writerId\": \"#{@writer_id}\", \"pid\": \"#{pid}\", \"email\": \"#{user}\", \"role\": \"#{role}\" }"
-        
+
         response = RestClient.post "#{@api_endpoint}/project-users", values, headers
-        
+
         return response
-        
+
     end
-    
+
     # DEPRECATED deactivate user in GoodData project
     def deactivate_user_old(uid, pid)
-        
+
         #user_id = "f6059d2cd367193ac21f1af2b639a78f"
-        
+
         headers  = {:x_storageapi_token => @kbc_api_token, :accept => :json, :content_type => :json}
-        
+
         query =     "/gdc/projects/#{pid}/users"
-        
+
         payload = "{\"user\": { \"content\": { \"status\": \"DISABLED\", \"userRoles\": [ \"/gdc/projects/#{pid}/roles/2\" ]}, \"links\": {\"self\": \"/gdc/account/profile/#{uid}\"}}}"
-        
+
         values = "{\n \"writerId\": \"#{@writer_id}\",\n \"query\": \"#{query}\",\n \"payload\": #{payload}\n}"
-        
+
         response = RestClient.post "#{@api_endpoint}/proxy", values, headers
-        
+
         return response
-        
+
     end
-    
+
     # deactivate user using built in KBC feature on API
     def deactivate_user(user, pid)
-        
+
         headers  = {:x_storageapi_token => @kbc_api_token, :accept => :json, :content_type => :json}
-        
+
         resource = ("#{@api_endpoint}/project-users?writerId=#{@writer_id}&pid=#{pid}&email=#{user}")
-        
+
         resource.gsub!("+", "%2B")
-        
+
         response = RestClient.delete resource, headers
-        
+
         return response
-        
+
     end
-    
+
     # activate existing user in GoodData project (only for users within your domain)
     def activate_user
-        
+
         #user_id = "f6059d2cd367193ac21f1af2b639a78f"
-        
+
         headers  = {:x_storageapi_token => @kbc_api_token, :accept => :json, :content_type => :json}
-        
+
         query = "/gdc/projects/#{pid}/users"
-        
+
         payload = "{\"user\": { \"content\": { \"status\": \"ENABLED\", \"userRoles\": [ \"/gdc/projects/#{pid}/roles/2\" ]}, \"links\": {\"self\": \"/gdc/account/profile/#{uid}\"}}}"
-        
+
         values = "{\n \"writerId\": \"#{@writer_id}\",\n \"query\": \"#{query}\",\n \"payload\": #{payload}\n}"
-        
+
         response = RestClient.post "#{@api_endpoint}/proxy", values, headers
-        
+
         return response
-        
+
     end
-    
+
     # apply changes for users
     def set_users
-        
+
     end
-    
+
     # method for saving result in SAPI (log eventu)
     def save_output
-        
+
     end
-    
+
     # method to assign user values for existing variables (using GoodData Ruby Automation SDK)
     def set_existing_variable_dev(project, var_title, var_values, user)
-        
+
         # assign to username
         username = $gd_username
         password = $gd_password
-        
+
         $client = GoodData.connect(username, password)
-        
+
         project = $client.projects(project)
-        
+
         var = project.variables.find { |v| v.title == var_title}
-        
+
         var_attr = var.content['attribute']
-        
+
         attribute = project.attributes(var_attr)
-        
+
         label = attribute.primary_label
-        
+
         values = var_values.split(",")
-        
+
         filters = []
-        
+
         filters.push([user, label] + values)
-        
+
         puts filters
-        
+
         project.add_variable_permissions(filters, var)
 
         GoodData.disconnect
-        
+
     end
-    
+
     def set_existing_variable_bulk(csv, project)
-        
+
         # assign to username
         username = $gd_username
         password = $gd_password
-        
+
         $client = GoodData.connect(username, password)
-        
+
         project = $client.projects(project)
-        
+
         array = []
         CSV.foreach(csv, :headers => true, :encoding => 'utf-8') do |row|
-            
+
             array << row['variable']
-            
+
             array.uniq
-            
+
         end
-        
+
         array.each do |vrr|
-            
+
             var = project.variables.find { |v| v.title == vrr}
-            
+
             var_attr = var.content['attribute']
-            
+
             attribute = project.attributes(var_attr)
-            
+
             label = attribute.primary_label
-            
+
             filters = []
-            
-            
+
+
             CSV.foreach(csv, :headers => true, :encoding => 'utf-8') do |row|
                 if row['variable'] == vrr
                     then
-                    
+
                     values = row['values'].split(",")
-                    
+
                     filters.push([row['user'], label] + values)
-                    
+
                 end
             end
-            
-            project.add_variable_permissions(filters, var)
-            
-        end
-        
-        GoodData.disconnect
-        
-    end
-    
 
-    
+            project.add_variable_permissions(filters, var)
+
+        end
+
+        GoodData.disconnect
+
+    end
+
+
+
 end
--- main.rb
+++ main.rb
@@ -2,9 +2,9 @@ require './lib/wrapper'
 
 options = {}
 OptionParser.new do |opts|
-    
+
     opts.on('-d', '--data DAT', 'Data') { |v| options[:data] = v }
-    
+
 end.parse!
 
 if options[:data].nil?
@@ -16,139 +16,139 @@ end
 manager = UMan.new(options)
 
 CSV.foreach(options[:data] + '/in/tables/users.csv', :headers => true, :encoding => 'utf-8') do |csv|
-    
+
     case csv['action']
         when "DISABLE"
-        
+
         #usrs = JSON.parse(manager.get_users())['users']
         #   filtered = usrs.select { |u| u['email'] == csv['user'] }
         #   uid = filtered[0]['uid']
         # result = manager.deactivate_user(uid,csv['pid'])
-        
+
             result = manager.deactivate_user(csv['user'],csv['pid'])
-            
+
             job_uri = JSON.parse(result)["url"]
-            
+
             headers  = {:x_storageapi_token => ENV["KBC_TOKEN"], :accept => :json, :content_type => :json}
-            
+
             finished = false
             until finished
                 res = RestClient.get job_uri, headers
                 finished  = JSON.parse(res)["isFinished"]
             end
-            
+
             job_status = JSON.parse(res)["status"]
             message = JSON.parse(res)["result"][0]
-            
+
             job_id = JSON.parse(result)["job"]
-            
+
             CSV.open($out_file.to_s, "ab") do |status|
-                status << [csv['user'], job_id, job_status, "DISABLE", Time.now.getutc]
+                status << [csv['user'], job_id, job_status, "DISABLE", Time.now.getutc, ""]
             end
-        
-        
+
+
         when "ENABLE"
-        
+
             if (csv['sso_provider'].to_s != '')
                 then
-                
+
                     # create user with sso provider
                     result = manager.create_user(csv['user'], SecureRandom.hex.to_s, csv['firstname'], csv['lastname'], csv['sso_provider'])
-                
+
                     job_uri = JSON.parse(result)["url"]
-                
+
                     headers  = {:x_storageapi_token => ENV["KBC_TOKEN"], :accept => :json, :content_type => :json}
-                
+
                     finished = false
                     until finished
                         res = RestClient.get job_uri, headers
                         finished  = JSON.parse(res)["isFinished"]
                     end
-                
+
                     job_status = JSON.parse(res)["status"]
                     message = JSON.parse(res)["result"][0]
-                
+
                     job_id = JSON.parse(result)["job"]
-                    
+
                     CSV.open($out_file.to_s, "ab") do |status|
-                        status << [csv['user'], job_id, job_status, "CREATE", Time.now.getutc]
+                        status << [csv['user'], job_id, job_status, "CREATE", Time.now.getutc, ""]
                     end
-                    
-                    
+
+
                     if job_status != 'error' then
-                    
+
                         # push the new user to the project directly
                         result = manager.add_to_project(csv['user'],csv['role'],csv['pid'])
-                    
+
                         job_uri = JSON.parse(result)["url"]
-                    
+
                         headers  = {:x_storageapi_token => ENV["KBC_TOKEN"], :accept => :json, :content_type => :json}
-                    
+
                         finished = false
                         until finished
                             res = RestClient.get job_uri, headers
                             finished  = JSON.parse(res)["isFinished"]
                         end
-                    
+
                         job_status = JSON.parse(res)["status"]
                         message = JSON.parse(res)["result"][0]
-                    
+
                         job_id = JSON.parse(result)["job"]
-                    
+
                         CSV.open($out_file.to_s, "ab") do |status|
-                            status << [csv['user'], job_id, job_status, "ADD", Time.now.getutc]
+                            status << [csv['user'], job_id, job_status, "ADD", Time.now.getutc, csv['role']]
                         end
                     end
 
                 else
-                
+
                     result = manager.add_to_project(csv['user'],csv['role'],csv['pid'])
-            
+
                     job_uri = JSON.parse(result)["url"]
-            
+
                     headers  = {:x_storageapi_token => ENV["KBC_TOKEN"], :accept => :json, :content_type => :json}
-            
+
                     finished = false
                     until finished
                         res = RestClient.get job_uri, headers
                         finished  = JSON.parse(res)["isFinished"]
                     end
-            
+
                     job_status = JSON.parse(res)["status"]
                     message = JSON.parse(res)["result"][0]
-            
+
                     job_id = JSON.parse(result)["job"]
-            
+
                     CSV.open($out_file.to_s, "ab") do |status|
-                        status << [csv['user'], job_id, job_status, "ENABLE", Time.now.getutc]
+                        status << [csv['user'], job_id, job_status, "ENABLE", Time.now.getutc, csv['role']]
                     end
-                    
+
             end
-        
+
         else
             puts "ERROR: no action specified for #{csv['user']}"
             exit 1
     end
-    
+
 end
 
 puts 'User provisioning finished.'
 
 if ($set_variables == 'true') then
-    
+
     puts "I'm setting variable now..."
-    
+
     variable_file = options[:data] + '/in/tables/variables.csv'
-    
+
     manager.set_existing_variable_bulk(variable_file,$gd_pid)
-    
+
     #CSV.foreach(options[:data] + '/in/tables/variables.csv', :headers => true) do |row|
     #if row['user'] == csv['user']
     #        then
     #            manager.set_existing_variable(csv['pid'], row['variable'], row['values'], row['user'])
     #        end
     #end
-    
+
 end
 
 exit 0
